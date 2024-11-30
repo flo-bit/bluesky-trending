beff4d56485015b039d1e55471bb3d5d09473f26
Florian <45694132+flo-bit@users.noreply.github.com>
2024-11-30 02:29:10 +0100

    remove api

--- src/routes/api/languages.json/+server.ts
+++
@@ -1,8 +0,0 @@
-import { languageEmojis } from '$lib/emojis';
-import { json, type RequestHandler } from '@sveltejs/kit';
-
-export const GET: RequestHandler = async () => {
-	const languages = Object.keys(languageEmojis);
-
-	return json({ languages });
-};
